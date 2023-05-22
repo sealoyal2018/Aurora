c2a93968bf516190c328b1fc62b7f83a030e7f58
sealoyal <sealoyal@hotmail.com>
2023-05-22 17:59:52 +0800

    新增：登录页面

---
+++ src/Aurora.WebApp/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aurora.WebApp.Controllers;
+
+public class AuthController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
